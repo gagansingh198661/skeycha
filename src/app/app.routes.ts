160257178941039f30198cd39c77d3e214a417c4
Gagan <gagan_singh198661@yahoo.com>
2026-06-17 23:49:17 +0530

    aboutus page working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Catalog } from './catalog/catalog';
 import { Productpage } from './productpage/productpage';
 import { CheckoutPage } from './checkout-page/checkout-page';
 import { Refunds } from './refunds/refunds';
+import { AboutUs } from './about-us/about-us';
 export const routes: Routes = [
   { path: '', component: Catalog },          // default route
   { path: 'product', component: Productpage }, // product detail route
   { path: 'checkout', component: CheckoutPage }, // checkout route
   { path: 'refunds', component: Refunds }, // refunds route
+  { path: 'aboutUs',component:AboutUs}
 ];
